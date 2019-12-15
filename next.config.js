9291eac0a0feba6ae1430f9115cfe0a3483d3e39
Roman <kotenkoroman2019@gmail.com>
2019-12-15 21:05:38 +0200

    attempt to deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 //
 
-const isProduction = process.env.NODE_ENV === 'production';
-
 module.exports = {
-  assetPrefix: isProduction ? '/next-hello-world' : '',
-  publicRuntimeConfig: {
-    // used in '/components/Link.js/', for more details go to the component itself
-    linkPrefix: isProduction ? '/next-hello-world' : ''
-  }
+  distDir: 'build'
 };
